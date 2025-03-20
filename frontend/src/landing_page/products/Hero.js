18c9101a261853115d914c9c9093407bc54cfd68
mchauhan208867 <mchauhan208867@gmail.com>
2025-03-20 23:41:13 +0530

    Re-added frontend and dashboard

---
+++ frontend/src/landing_page/products/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="container ">
+      <div className="row mt-5 text-center">
+        <h1 className="mb-2 " >Zerodha Products</h1>
+        <h4 className='mb -2 text-muted'>Sleek, modern, and intuitive trading platforms</h4>
+        <h6>Check out our <a href='#'> investment offerings <i class="fa fa-arrow-right" aria-hidden="true"></i> </a> </h6>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Hero

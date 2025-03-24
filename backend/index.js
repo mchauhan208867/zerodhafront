6e69b9e09d3ef386b30d33514c754a2b8f2d7d88
mchauhan208867 <mchauhan208867@gmail.com>
2025-03-25 00:00:13 +0530

    adding buy window

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+
 
 const PORT = process.env.PORT || 3002;
 const uri = process.env.MONGO_URL;
@@ -198,6 +200,20 @@ app.use(bodyParser.json());
           let allPositions =  await PositionsModel.find({});
           res.json(allPositions);
         });
+
+        app.post("/newOrder", async (req, res) => {
+          let newOrder = new OrdersModel({
+            name: req.body.name,
+            qty: req.body.qty,
+            price: req.body.price,
+            mode: req.body.mode,
+          });
+        
+          newOrder.save();
+        
+          res.send("Order saved!");
+        });
+        
         
         app.listen(PORT, () => {
           console.log("App started!");

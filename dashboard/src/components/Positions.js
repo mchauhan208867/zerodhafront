48b5ad23111dd1783157aedbf573c87f76735fe9
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-02 16:04:00 +0530

    Update Positions.js

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -7,7 +7,7 @@ const Positions = () => {
    const [allPositions ,  setAllPositions ] = useState([]);
   
     useEffect(() => {
-      axios.get("http://localhost:3002/allPositions").then((res) => {
+      axios.get("https://zerodhafront-1.onrender.com/allPositions").then((res) => {
         // console.log(res.data);
         setAllPositions(res.data);
       });

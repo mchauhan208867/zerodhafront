28572802bd0dd8d55e7b7c0a7d0833794fdceccc
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 12:48:56 +0530

    Update RightSection.js

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -2,18 +2,23 @@ import React from "react";
 
 function RightSection({ imageURL, productName, productDesription, learnMore }) {
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-6 p-5 mt-5">
-          <h1>{productName}</h1>
-          <p>{productDesription}</p>
-          <div>
-            <a href={learnMore}>Learn More</a>
-          </div>
+    <div className="container my-5">
+      <div className="row align-items-center">
+
+        {/* Text Section */}
+        <div className="col-md-6 p-4">
+          <h1 className="mb-3">{productName}</h1>
+          <p className="mb-4">{productDesription}</p>
+          <a href={learnMore} className="btn btn-primary">
+            Learn More
+          </a>
         </div>
-        <div className="col-6">
-          <img src={imageURL} />
+
+        {/* Image Section */}
+        <div className="col-md-6 text-center">
+          <img src={imageURL} alt="Product" className="img-fluid" />
         </div>
+
       </div>
     </div>
   );

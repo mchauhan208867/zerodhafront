01d3067b1ba61dfb13dc04d51f0de3a670467b19
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 12:49:20 +0530

    Update Universe.js

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -2,42 +2,30 @@ import React from "react";
 
 function Universe() {
   return (
-    <div className="container mt-5">
+    <div className="container my-5">
       <div className="row text-center">
-        <h1>The Zerodha Universe</h1>
-        <p>
+        <h1 className="mb-3">The Zerodha Universe</h1>
+        <p className="text-muted">
           Extend your trading and investment experience even further with our
           partner platforms
         </p>
+      </div>
 
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <div className="col-4 p-3 mt-5">
-          <img src="media/images/smallcaseLogo.png" />
-          <p className="text-small text-muted">Thematic investment platform</p>
-        </div>
-        <button
-          className="p-2 btn btn-primary fs-5 mb-5"
-          style={{ width: "20%", margin: "0 auto" }}
-        >
+      <div className="row text-center mt-4">
+        {[...Array(6)].map((_, index) => (
+          <div className="col-md-4 p-4" key={index}>
+            <img
+              src="media/images/smallcaseLogo.png"
+              alt="Partner Platform"
+              className="img-fluid mb-3"
+            />
+            <p className="text-muted">Thematic investment platform</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="d-flex justify-content-center mt-4 mb-5">
+        <button className="btn btn-primary fs-5 px-4">
           Signup Now
         </button>
       </div>

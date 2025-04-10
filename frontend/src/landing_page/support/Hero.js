9f600a76d87ff3c2b2f8b2abf34c4054275c70db
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-10 12:56:17 +0530

    Update Hero.js

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,31 +2,41 @@ import React from "react";
 
 function Hero() {
   return (
-    <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+    <section className="container-fluid bg-light" id="supportHero">
+      <div className="p-5 bg-primary text-white" id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <a href="" className="text-white text-decoration-underline">
+          Track Tickets
+        </a>
       </div>
-      <div className="row p-5 m-3">
-        <div className="col-6 p-3">
-          <h1 className="fs-3">
+
+      <div className="row px-5 py-4 mx-3">
+        <div className="col-md-6 p-3">
+          <h1 className="fs-3 mb-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
-          <input placeholder="Eg. how do I activate F&O" />
-          <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <input
+            type="text"
+            className="form-control mb-4"
+            placeholder="Eg. how do I activate F&O"
+          />
+
+          <div className="d-flex flex-column gap-2">
+            <a href="#">Track account opening</a>
+            <a href="#">Track segment activation</a>
+            <a href="#">Intraday margins</a>
+            <a href="#">Kite user manual</a>
+          </div>
         </div>
-        <div className="col-6 p-3">
-          <h1 className="fs-3">Featured</h1>
-          <ol>
-            <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
+
+        <div className="col-md-6 p-3">
+          <h1 className="fs-3 mb-3">Featured</h1>
+          <ol className="ps-3">
+            <li className="mb-2">
+              <a href="#">Current Takeovers and Delisting - January 2024</a>
             </li>
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <a href="#">Latest Intraday leverages - MIS & CO</a>
             </li>
           </ol>
         </div>

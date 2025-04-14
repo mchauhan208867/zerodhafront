740c7fd831a2950a0635d26145e4f5047944ca2b
mchauhan208867 <127039437+mchauhan208867@users.noreply.github.com>
2025-04-14 12:45:51 +0530

    Update Summary.js

--- dashboard/src/components/Summary.js
+++ dashboard/src/components/Summary.js
@@ -18,9 +18,7 @@ const Summary = () => {
       </div>
 
       <div className="section">
-        <span>
-          <p>Equity</p>
-        </span>
+        <span><p>Equity</p></span>
 
         <div className="data">
           <div className="first">
@@ -30,38 +28,26 @@ const Summary = () => {
           <hr />
 
           <div className="second">
-            <p>
-              Margins used <span>0</span>
-            </p>
-            <p>
-              Opening balance <span>3.74k</span>
-            </p>
+            <p>Margins used <span>0</span></p>
+            <p>Opening balance <span>3.74k</span></p>
           </div>
         </div>
         <hr className="divider" />
       </div>
 
       <div className="section">
-        <span>
-          <p>Holdings (13)</p>
-        </span>
+        <span><p>Holdings (13)</p></span>
 
         <div className="data">
           <div className="first">
-            <h3 className="profit">
-              1.55k <small>+5.20%</small>
-            </h3>
+            <h3 className="profit">1.55k <small>+5.20%</small></h3>
             <p>P&L</p>
           </div>
           <hr />
 
           <div className="second">
-            <p>
-              Current Value <span>31.43k</span>
-            </p>
-            <p>
-              Investment <span>29.88k</span>
-            </p>
+            <p>Current Value <span>31.43k</span></p>
+            <p>Investment <span>29.88k</span></p>
           </div>
         </div>
         <hr className="divider" />
